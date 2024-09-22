f3c0e7e38a0ac85b62fea95f6959b81add30d9ad
Seu Nome <seu-email@example.com>
2024-09-21 22:23:33 -0300

    'mudanças'

--- apimercadopago.py
+++ apimercadopago.py
@@ -2,13 +2,16 @@
 import mercadopago
 
 def gerar_link_pagamento():
-    sdk = mercadopago.SDK("APP_USR-1746040795185388-092019-6d447169a49a891e0dec99d562fb1104-310155133")
+    sdk = mercadopago.SDK("APP_USR-959358377413846-091918-f382a54b75e515fe89c7aea46d38c6c0-1956623115")
+    #APP_USR-959358377413846-091918-f382a54b75e515fe89c7aea46d38c6c0-1956623115
+    #Produção: APP_USR-1746040795185388-092019-6d447169a49a891e0dec99d562fb1104-310155133
 
     payment_data = {
         "items": [
-            {"id": "1", "title": "App Sjurados Vitalício", "quantity": 1, "currency_id": "BRL", "unit_price": 1.00}
+            {"id": "1", "title": "App Sjurados Vitalício", "quantity": 1, "currency_id": "BRL", "unit_price": 50.00}
         ],
         "back_urls": {
+            #"success": "https://nameless-dawn-62388-3d072fabd70a.herokuapp.com/checkout_success",
             "success": "https://nameless-dawn-62388-3d072fabd70a.herokuapp.com/cadastro",
             "failure": "https://nameless-dawn-62388-3d072fabd70a.herokuapp.com/compraerrada",
             "pending": "https://nameless-dawn-62388-3d072fabd70a.herokuapp.com/compraerrada",
